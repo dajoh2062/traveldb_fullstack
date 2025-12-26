97f98f8d96a8f8d0b2d33af7d5b477c1759375d8
dajoh2062 <dajoh2062@oslomet.no>
2025-12-26 17:16:48 +0100

    Initial setup

---
+++ traveldbBackend/src/main/java/projects/traveldbbackend/TraveldbBackendApplication.java
@@ -0,0 +1,13 @@
+package projects.traveldbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveldbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraveldbBackendApplication.class, args);
+    }
+
+}
---
+++ traveldbBackend/src/test/java/projects/traveldbbackend/TraveldbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.traveldbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveldbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
